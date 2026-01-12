3b296035dde5d9e2eee8d2f95619e922ab013543
nhannguyen95 <nhannguyenmath95@gmail.com>
2026-01-12 17:52:45 +0900

    Setup self-updating CICD pipeline stack

---
+++ lib/stacks/pipeline.ts
@@ -0,0 +1,23 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, ShellStep, CodePipelineSource } from 'aws-cdk-lib/pipelines';
+
+export class PipelineStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'zeus-frontend-pipeline',
+            synth: new ShellStep('Synth', {
+                input: CodePipelineSource.connection('nhannguyen95/zeus-frontend-cdk', 'main', {
+                    connectionArn: 'arn:aws:codeconnections:us-east-1:533389119429:connection/42d60ce7-88e9-407d-86e4-71714f351907',
+                }),
+                commands: [
+                    'npm install',
+                    'npm run build',
+                    'npm run cdk synth'
+                ]
+            })
+        });
+    }
+}
---
+++ test/zeus-frontend-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ZeusFrontendCdk from '../lib/zeus-frontend-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/zeus-frontend-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ZeusFrontendCdk.ZeusFrontendCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
